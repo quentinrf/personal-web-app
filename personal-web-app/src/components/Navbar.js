d040f28db947365c8a519781a9e63aa7a5c27b9e
Quentin Roy-Foster <quentinrf@gmail.com>
2021-01-29 12:53:21 -0500

    testing new github token

--- personal-web-app/src/components/Navbar.js
+++ personal-web-app/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
+//test comment 
 
 function Navbar() {
     const [click, setClick] = useState(false);
