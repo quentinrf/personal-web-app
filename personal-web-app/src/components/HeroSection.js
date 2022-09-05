9236ab3113253f1e825350c5eee50029ab77c73a
Quentin Roy-Foster <quentin_royfoster@trendmicro.com>
2022-09-05 12:09:30 -0400

    removed unused components

--- personal-web-app/src/components/HeroSection.js
+++ personal-web-app/src/components/HeroSection.js
@@ -6,13 +6,8 @@ import { Button } from './Button';
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src="/videos/video-2.mp4" autoPlay loop muted />
             <h1>Software Engineer</h1>
             <p>Quentin Roy-Foster</p>
-            <div className="hero-btns">
-                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
-                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>WATCH TRAILER <i className='far fa-play-circle'/></Button>
-            </div>
         </div>
     )
 }
